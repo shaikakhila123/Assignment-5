63c3c1ff08b2198162b689986dcbb40749853bd7
shaikakhila123 <139138283+shaikakhila123@users.noreply.github.com>
2023-08-16 17:56:44 +0530

    Assignment-5

---
+++ src/app/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@angular/core";
+import { CounterService } from "./counter.service";
+
+@Injectable()
+export class UserService{
+   activeUsers=['A','B'];
+   inactiveUsers=['C','D']; 
+
+   constructor(private counterService:CounterService){
+
+   }
+
+    setToActive(id:number){
+        this.activeUsers.push(this.inactiveUsers[id]);
+        this.inactiveUsers.splice(id,1);
+        this.counterService.incrementInactiveToActive();
+
+
+    }
+    setToInactive(id:number){
+        this.inactiveUsers.push(this.activeUsers[id]);
+        this.activeUsers.splice(id,1);
+        this.counterService.incrementActiveToInactive();
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
